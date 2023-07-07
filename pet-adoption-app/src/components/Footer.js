17325ff388d2ed760fe7cfa91804f016d1987fab
RamyBenChaalia <chaaliaramy@gmail.com>
2023-07-07 15:38:29 +0100

    starting-with-frontend

---
+++ pet-adoption-app/src/components/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-light">
+      <div className="container py-3">
+        <div className="row">
+          <div className="col-lg-6">
+            <ul className="list-inline mb-0">
+              <li className="list-inline-item">
+                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-6 text-lg-right">
+            <span className="text-muted">About Us</span>
+            <span className="mx-2 text-muted">|</span>
+            <span className="text-muted">Contact Us</span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
---
+++ pet-adoption-app/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container">
+      <h2 className="mt-4">Mission</h2>
+      <p className="lead">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque suscipit, dui sit amet blandit pellentesque, justo neque tincidunt augue, et luctus est metus at dui. Sed in est at justo elementum eleifend. Ut varius mi at tortor consequat, at lobortis mauris dignissim. Integer suscipit nulla a leo venenatis eleifend. Aliquam eget turpis eget lectus placerat lacinia. Ut auctor tincidunt dui, eget faucibus ligula dignissim in.
+      </p>
+      <p>
+        Proin venenatis magna et diam placerat finibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla tristique tincidunt neque, eget rutrum est faucibus sit amet. Fusce id malesuada felis, non finibus massa. Fusce at fringilla leo. Curabitur et commodo neque. Curabitur vulputate purus sed diam dapibus, non tempor nulla egestas. Vivamus a nunc a nisl interdum consequat. Aenean sed metus ut est vulputate iaculis.
+      </p>
+    </div>
+  );
+}
+
+export default Home;

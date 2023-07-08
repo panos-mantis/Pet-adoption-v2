9058aba81af58d0f4e62f467c4dfd773d52f5c44
RamyBenChaalia <chaaliaramy@gmail.com>
2023-07-08 14:32:55 +0100

    admins

--- pet-adoption-app/src/components/LogIn.js
+++ pet-adoption-app/src/components/LogIn.js
@@ -1,73 +1,83 @@
-import React from "react";
-import { useState } from "react";
-import {Link} from "react-router-dom"
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { loginUser } from "../api";
 
 axios.defaults.baseURL = "http://localhost:4000/api";
 
 const LogIn = () => {
-  //States
-  
+  const navigate = useNavigate(); // Get the navigate function
+
+  // States
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  
   const handleChangeEmail = (e) => {
     setEmail(e.target.value);
-    console.log(email);
   };
+
   const handleChangePassword = (e) => {
     setPassword(e.target.value);
-    console.log(password);
   };
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
-
       const credentials = {
         email,
-        password
+        password,
       };
 
-    await loginUser(credentials);
-    }catch(error){
-        console.log(error)
+      await loginUser(credentials);
+      navigate("/"); // Navigate to the home page
+    } catch (error) {
+      console.log(error);
     }
-  }
-  
+  };
+
   return (
-    <main>
-        <h1>Log In</h1>
-    <form onSubmit={handleLogin}>
-      <div>
-        <label className="">email</label>
-        <input
-          className=""
-          type="text"
-          placeholder="Enter your email here"
-          value={email}
-          /* required */ onChange={(e) => handleChangeEmail(e)}
-        ></input>
-      </div>
-      <div>
-        <label className="">password</label>
-        <input
-          className=""
-          type="password"
-          placeholder="Enter your password here"
-          value={password}
-          /* required */ onChange={(e) => handleChangePassword(e)}
-        ></input>
-      </div>
-      <button className=''>Log in</button>
-    </form>
-    <p>Don't have an account yet? click <Link to="/Register" >Register</Link></p>
+    <main className="container">
+      <h1 className="mt-4">Log In</h1>
+      <form onSubmit={handleLogin}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={handleChangeEmail}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={handleChangePassword}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Log In
+        </button>
+      </form>
+      <p className="mt-3">
+        Don't have an account yet? Click <Link to="/Register">here</Link> to
+        register.
+      </p>
     </main>
-    
   );
 };
 
 export default LogIn;
+
+
